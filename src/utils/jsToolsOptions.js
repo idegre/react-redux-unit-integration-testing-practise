edef8a0acfebcabffce83b55cd81ef592808e42f
ignacio degregori <ignacio.degregori@gmail.com>
2019-04-02 16:07:31 -0300

    added options array

---
+++ src/utils/jsToolsOptions.js
@@ -0,0 +1,14 @@
+export default [
+    'ReactJS',
+    'Webpack',
+    'AngularJS',
+    'Yarn',
+    'NPM',
+    'ExpressJS',
+    'NodeJS',
+    'JQuery',
+    'Redux',
+    'Hooks',
+    'VueJS',
+    'Grunt',
+]
